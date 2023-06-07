6fd8f02b6f3224f24e46739e48e6b957a76269ca
R0BC0D3R <R0BC0D3R@gmail.com>
2023-06-07 15:41:22 -0400

    Include user changes

--- Nerva.Bots/Src/Classes/DiscordUser.cs
+++ Nerva.Bots/Src/Classes/DiscordUser.cs
@@ -5,9 +5,11 @@ namespace Nerva.Bots.Classes
 {
     public class DiscordUser
     {
+        public ulong Id { get; set; }
         public string UserName { get; set; }
         public string Discriminator { get; set; }
-        public IList<string> Roles { get; set; }
+        public DateTime JoinedDate { get; set; }
+        public IList<ulong> Roles { get; set; }
         public DateTime LastPostDate { get; set; }
         public DateTime WarnedDate { get; set; }
     }
